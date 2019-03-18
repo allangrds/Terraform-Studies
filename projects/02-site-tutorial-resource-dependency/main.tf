823ebaed9c598e7d2e99ec15cc59c2cb03cfd85c
Allan Ramos <allangrds@gmail.com>
2019-03-18 19:57:42 -0300

    terraform: add tutorial step - resource dependency

--- projects/02-site-tutorial-resource-dependency/main.tf
+++ projects/02-site-tutorial-resource-dependency/main.tf
@@ -1,9 +1,14 @@
 provider "aws" {
-  access_key = "YOUR_SECRET_KEY"
+  access_key = "YOUR_ACCESS_KEY"
+  secret_key = "YOUR_SECRET_KEY"
   region     = "us-east-1"
 }
 
 resource "aws_instance" "first_instance" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.first_instance.id}"
 }
